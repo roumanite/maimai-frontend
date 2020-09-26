89cb234b6aad0f15168c3571220736110ae6fbec
Madyarini <madyagra@gmail.com>
2020-09-26 15:01:17 +0800

    Use webpack dev server

---
+++ src/MaimaiApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MaimaiApp extends React.Component {
+  render() {
+    return (
+      <div>
+        Welcome to Maimai
+      </div>
+    );
+  }
+}
